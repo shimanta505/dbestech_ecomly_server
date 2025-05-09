6e1c9beb50c042545c20bd250268035d1c924097
shimanta505 <sarkershimanta@gmail.com>
2025-05-09 13:58:34 +0600

    auth req success

--- controllers/auth_controller.js
+++ controllers/auth_controller.js
@@ -1,6 +1,43 @@
-exports.register =async function(req,res){};
+const express = require('express');
 
-exports.login = async function(req,res){};
+const {validationResult} = require('express-validator');
+const {User} = require('../models/user');
+const bcrypt = require('bcryptjs');
+
+exports.login =async function(req,res){return res.status(201);};
+
+exports.register = async function(req,res){
+    const errors = validationResult(req);
+
+    if(!errors.isEmpty()){
+        console.log(errors);
+        const errorMessages = errors.array().map((error)=> 
+        ({field: error.path,message: error.message})
+        );
+        return res.status(400).json({errors: errorMessages});
+    }
+    else {
+        try{
+            let user = new User({...req.body,passwordHash: bcrypt
+                .hashSync(req.body.password, 8)});
+           user = await user.save();
+           if(!user){
+            return res.status(500).json({type: 'Internal Server Error',
+                message: 'couldnot created a new user '
+            });
+           }
+           return res.status(201).json(user);
+        }catch(error){
+            if(error.message.includes('email_1 dup key')){
+                return res.status(409).json({
+                    type: 'AuthError',
+                    message: 'user with that email already exists'
+                });
+            }
+            return res.status(500).json({type: error.name,message: error.message});
+        }
+    }
+};
 exports.forgotPassword = async function(req,res){};
 exports.verifyPasswordResetOtp = async function(req,res){};
 exports.resetPassword = async function(req,res){};
--- routes/auth.js
+++ routes/auth.js
@@ -3,12 +3,19 @@ const router = express.Router();
 
 const authController = require('../controllers/auth_controller');
 
-router.post('/login',(req,res)=> authController.login());
+const {body} = require('express-validator');
 
+const validateUser = [
+    body('name').not().isEmpty().withMessage('Name is required'),
+    body('email').isEmail().withMessage('please enter a valid email address'),
+    body('password').isLength({min: 8}).withMessage('password must be at least 8 characters')
+    .isStrongPassword().withMessage('password must be contain at least one uppercase,one lowercase and one sybbol'),
+    body('phone').isMobilePhone().withMessage('please enter a valid phone number')
+];
 
-router.post('/register',(req,res)=>
-    authController.register()
-);
+router.post('/login', authController.login);
+
+router.post('/register',validateUser, authController.register);
 
 router.post('/forgot-password',(req,res)=>
     authController.forgotPassword()
